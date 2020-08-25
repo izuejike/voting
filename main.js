234daf29c5a2fd298941f2d6a1efafbcfd661a57
izu <izuejike1@gmail.com>
2020-08-25 07:45:14 -0700

    voting age

---
+++ main.js
@@ -0,0 +1,17 @@
+// console.log('Hello World');
+// console.error('This is an error')
+// console.warn('This is a warning')
+// let age = 30
+// age = 31
+// console.log(age)
+// var x;
+// 		x = prompt("Hello Please input your name");
+// 		y= prompt('Hey '+x+' Whats your year of birth?')
+// 		alert('Hi '+x+' nice to meet you')
+
+var x = prompt("Please Enter Your Name")
+var y = prompt("Please Enter Your Year Of Birth")
+var z = 2020 - y;
+
+	z >=18? alert('Hello,'+ x + "You are eligible to vote"):
+	alert("sorry you are not legible thank you");
